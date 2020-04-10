42b705d01e30351775ac39e16f2b513d566b54d5
Elijah <enutter24@gmail.com>
2020-04-10 05:28:14 -0700

    Setting everything up and adding assets

---
+++ Assets/Scripts/Game/GameManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public int lives = 3;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private bool canMove = true;
+    public LayerMask platformLayerMask;
+    public Rigidbody2D player;
+    public float speed = 3;
+    public Animator anim;
+    public float jumpPower = 3;
+
+    void Start()
+    {
+        player = this.GetComponent<Rigidbody2D>();
+        anim = this.GetComponentInChildren<Animator>();
+    }
+
+    void Update()
+    {
+        if(canMove)
+            Movement();
+        if(isGrounded() && Input.GetKeyDown(KeyCode.Space))
+            Jump();
+    }
+
+    void Movement()
+    {
+        float move = Input.GetAxis("Horizontal") * speed;
+
+        player.velocity = new Vector2(move, player.velocity.y);
+
+        anim.SetFloat("Speed", move);
+
+        if(player.velocity == Vector2.zero)
+            anim.SetBool("Running", false);
+        else
+            anim.SetBool("Running", true);
+    }
+
+    void Jump()
+    {
+        player.velocity = new Vector2(player.velocity.x, jumpPower);
+    }
+
+    bool isGrounded()
+    {
+        return Physics2D.Raycast(player.transform.position, Vector2.down, 0.25f, platformLayerMask);
+    }
+}
