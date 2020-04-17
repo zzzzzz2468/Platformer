4524b42c8624669332ed413bd28963f1289e5735
Elijah <enutter24@gmail.com>
2020-04-17 04:53:07 -0700

    Ugh

--- Assets/Scripts/Camera/CameraController.cs
+++ Assets/Scripts/Camera/CameraController.cs
@@ -4,14 +4,17 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    //sets variables
     public GameObject player;
     private Vector3 offset;
 
+    //detects player
     void Start()
     {
         offset = transform.position - player.transform.position;
     }
 
+    //Moves the camera
     void LateUpdate()
     {
         transform.position = player.transform.position + offset;
--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //sets the variables
     private bool canMove = true;
     public LayerMask platformLayerMask;
     public Rigidbody2D player;
@@ -13,18 +14,23 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
+        //gets the components
         player = this.GetComponent<Rigidbody2D>();
         anim = this.GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
+        //detects if player can move and then moves, in case i want to prevent player from moving
         if(canMove)
             Movement();
+
+        //detects if player is grounded and space is pressed, which allows the player to jump
         if(isGrounded() && Input.GetKeyDown(KeyCode.Space))
             Jump();
     }
 
+    //movement and setting move animations
     void Movement()
     {
         float move = Input.GetAxis("Horizontal") * speed;
@@ -39,11 +45,13 @@ public class PlayerController : MonoBehaviour
             anim.SetBool("Running", true);
     }
 
+    //player jumping
     void Jump()
     {
         player.velocity = new Vector2(player.velocity.x, jumpPower);
     }
 
+    //detects if player is grounded
     bool isGrounded()
     {
         return Physics2D.Raycast(player.transform.position, Vector2.down, 0.25f, platformLayerMask);
